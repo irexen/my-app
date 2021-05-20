84169c53f7a5c794510dac2088a243fe8d5f73d4
irexen <irexen77@yahoo.com>
2021-05-20 11:03:07 -0500

    Nombre del Proyecto: my-app, Descripción: versión final del proyecto my-app

---
+++ src/main/java/com/arv/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.arv.myapp.entity;
+
+/**
+ * @author Alejandro Rebollo
+ *
+ */
+public class TestEntity {
+	/** 
+	 * Propiedad de prueba  para muestra de compilación del archivo testentity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
